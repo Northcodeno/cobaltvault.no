d3b5eb11709d496ed72662ed8eebbe4795b5de4d
Andreas Larsen <northcode.no@gmail.com>
2016-01-24 02:54:53 +0100

    Work on index page

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.views.generic.list import ListView
+
 # Create your views here.
+def index(request):
+    return render(request, "mainpage/index.html", {})
