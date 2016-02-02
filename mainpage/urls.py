00a8b84b387530d8eefbd4dfa0ea8c28c1574c4d
Jenjen1324 <jens@northcode.no>
2016-02-03 00:27:09 +0100

    More work on projects
    idk what I'm doing

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^p/(?P<project_id>[0-9A-Za-z]+)/download$', views.project_download, name='project_download'),
     url(r'^p/(?P<project_id>[0-9A-Za-z]+)/$', views.project, name='project'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic.list import ListView
+from django.utils.encoding import smart_str
 
 from .models import Project
 
@@ -17,3 +18,15 @@ def project(request, project_id):
 		except Project.DoesNotExist:
 			raise Http404('Project does not exist!')
 	return render(request, "mainpage/project.html", {'project': project})
+
+def project_download(request, project_id):
+	try:
+		project = Project.objects.get(pk=project_id)
+	except:
+		try:
+			project = Project.objects.get(idname=project_id)
+		except Project.DoesNotExist:
+			raise Http404('Project does not exist!')
+	
+	#response = HttpResponse(mimetype='application/force-download')
+	#response['Content-Disposition'] = 'attachment; filename=%s' % smart_str()
