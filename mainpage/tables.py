e062ece4c7aa7e2a7d8d6d91f52ee1570cf51dee
Jenjen1324 <jens@northcode.no>
2016-02-07 17:17:45 +0100

    Fixed ordering

--- mainpage/tables.py
+++ mainpage/tables.py
@@ -2,13 +2,13 @@ import django_tables2 as tables
 from .models import Project
 
 class ProjectTable(tables.Table):
-	title = tables.Column(accessor='get_thumb', verbose_name='')
-	description = tables.Column(accessor='get_desc', verbose_name='Description', order_by='name')
+	title = tables.Column(accessor='get_thumb', verbose_name='', orderable=False)
+	description = tables.Column(accessor='get_desc', verbose_name='Name', order_by='name')
 	maptype = tables.Column(verbose_name='Type')
 	downloads = tables.Column(verbose_name='DL')
 	date_created = tables.Column(accessor='date_created.date', verbose_name='Date Created')
 	date_modified = tables.Column(accessor='date_modified.date', verbose_name='Last Update')
-	author = tables.Column(accessor='get_pretty_authors', verbose_name='Author')
+	author = tables.Column(accessor='get_pretty_authors', verbose_name='Author', orderable=False)
 
 	class Meta:
 		attrs = {'class': 'table'}
--- mainpage/views.py
+++ mainpage/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 from django.utils.encoding import smart_str
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
+from django_tables2 import RequestConfig
 
 from .models import Project
 from .tables import ProjectTable
@@ -11,14 +12,15 @@ from info.models import NewsPost
 
 # Create your views here.
 def index(request):
-	latest = Project.objects.order_by('date_created')[:5]
-	mostdl = Project.objects.order_by('downloads')[:5]
+	latest = Project.objects.order_by('-date_modified')[:5]
+	mostdl = Project.objects.order_by('-downloads')[:5]
 	news = NewsPost.objects.all()[:5]
 	return render(request, "mainpage/index.html", { 'latest': latest, 'mostdl': mostdl, 'news': news })
 
 def list(request):
 	table = ProjectTable(Project.objects.all())
 	table.paginate(page=request.GET.get('page',1), per_page=25)
+	RequestConfig(request).configure(table)
 	return render(request, "mainpage/list.html", {'table': table })
 
 def project(request, project_id):
