051489c31097e6fc4e594fa3e9d2795152d0f9cc
Jenjen1324 <jens@northcode.no>
2016-02-07 17:20:18 +0100

    Fixed fix table ordering

--- mainpage/tables.py
+++ mainpage/tables.py
@@ -6,8 +6,8 @@ class ProjectTable(tables.Table):
 	description = tables.Column(accessor='get_desc', verbose_name='Name', order_by='name')
 	maptype = tables.Column(verbose_name='Type')
 	downloads = tables.Column(verbose_name='DL')
-	date_created = tables.Column(accessor='date_created.date', verbose_name='Date Created')
-	date_modified = tables.Column(accessor='date_modified.date', verbose_name='Last Update')
+	date_created = tables.Column(accessor='date_created.date', verbose_name='Date Created', order_by='date_created')
+	date_modified = tables.Column(accessor='date_modified.date', verbose_name='Last Update', order_by='date_modified')
 	author = tables.Column(accessor='get_pretty_authors', verbose_name='Author', orderable=False)
 
 	class Meta:
